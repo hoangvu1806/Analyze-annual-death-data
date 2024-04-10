0859e5ebdda7d0f4b48e6d65304b8efaab095b2b
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-10 23:33:14 +0700

    Updata main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
 
-file_path = "dataset.csv"
-df = pd.read_csv(file_path)
+df = pd.read_csv("dataset.csv")
+df_numeric = df.drop(['Year'], axis=1).select_dtypes(include=[np.number])
 
-print(df.describe())
+# For each column, calculate the IQR
+Q1 = df_numeric.quantile(0.25)
+Q3 = df_numeric.quantile(0.75)
+IQR = Q3 - Q1
+
+# Define a condition for outliers
+condition = (df_numeric >= (Q1 - 1.5 * IQR)) | (df_numeric <= (Q3 + 1.5 * IQR))
+
+# Find potential outliers
+potential_outliers = df_numeric[condition]
+
+# Print the potential outliers
+print(potential_outliers)
