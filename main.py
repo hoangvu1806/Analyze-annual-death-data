5369b8e6e1eaebebe01ce59f9b8bf2b1708693da
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-12 23:46:45 +0700

    Updata main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 df = pd.read_csv("dataset.csv")
+df.columns = df.columns.str.replace(' fatalities', '')
+
 df_numeric = df.drop(['Year'], axis=1).select_dtypes(include=[np.number])
 
 # For each column, calculate the IQR
@@ -16,4 +19,16 @@ condition = (df_numeric >= (Q1 - 1.5 * IQR)) | (df_numeric <= (Q3 + 1.5 * IQR))
 potential_outliers = df_numeric[condition]
 
 # Print the potential outliers
-print(potential_outliers)
+# print(potential_outliers)
+
+correlation_matrix = df_numeric.corr().T
+# print(correlation_matrix)
+plt.figure(figsize=(14, 10))
+sns.heatmap(correlation_matrix, 
+            annot=True, 
+            fmt=".2f", 
+            cmap='seismic', 
+            cbar=True, 
+            square=True)
+plt.title('Heatmap of Correlation Matrix')
+print(correlation_matrix)
